a56c17ce15c42fbb3011b440ec3e32eec377907f
tsamarthakur515-spec <tsamarthakur515@gmail.com>
2026-03-19 14:55:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,14 @@
 import os
 from datetime import datetime
 
+# --- Bot Credentials ---
 API_ID = 33603336
 API_HASH = "c9683a8ec3b886c18219f650fc8ed429"
 BOT_TOKEN = "8411080834:AAE85QH-LpaiOpht-RSMpwYQPus1jHONnu4"
-SESSION_STRING = "BQE-4i0..."
+SESSION_STRING = "BQE-4i0..." # Apni session string yahan puri daal dena
 
-# Global Dictionaries
+# --- Global Tracking ---
 queues = {}
+playing_messages = {} # {chat_id: message_id} - Timer edit karne ke liye
+current_playing = {} # {chat_id: song_details} - Currently playing track ke liye
 BOT_START_TIME = datetime.now()
