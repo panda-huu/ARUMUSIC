0eeae43aa8cd9a966e85f73cdaf67d7db6684700
tsamarthakur515-spec <tsamarthakur515@gmail.com>
2026-03-19 15:13:57 +0530

    Update ping.py

--- ARUMUZIC/plugins/ping.py
+++ ARUMUZIC/plugins/ping.py
@@ -1,5 +1,6 @@
 import time
 import psutil
+from ARUMUZIC.clients import bot, assistant, call
 from datetime import datetime, timedelta
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
